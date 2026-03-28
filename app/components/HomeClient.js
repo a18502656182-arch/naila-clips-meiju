1a4885d0a6aba5883cb89ae91a688cef2d34fee6
a18502656182-arch <a18502656182@gmail.com>
2026-03-28 12:53:46 +0800

    Update HomeClient.js

--- app/components/HomeClient.js
+++ app/components/HomeClient.js
@@ -3,14 +3,18 @@
 import { useState } from "react";
 import FiltersClient from "./FiltersClient";
 import ClipsGridClient from "./ClipsGridClient";
+
 export default function HomeClient({ allItems, initialTaxonomies }) {
   const [filters, setFilters] = useState({
     sort: "newest",
     access: [],
     difficulty: [],
-    topic: [],
-    channel: [],
+    genre: [],
+    duration: [],
+    show: [],
+    showSearch: "",
   });
+
   return (
     <div>
       <FiltersClient filters={filters} onFiltersChange={setFilters} initialTaxonomies={initialTaxonomies} />
