6dfc597150e7405f2e72a7c911038e98fa4adbf8
a18502656182-arch <a18502656182@gmail.com>
2026-04-27 13:05:26 +0800

    Update BuyFloatBtn.js

--- app/components/BuyFloatBtn.js
+++ app/components/BuyFloatBtn.js
@@ -105,9 +105,9 @@ export default function BuyFloatBtn() {
           .buy-float-badge { display: none; }
         }
       `}</style>
-      <div className="buy-float-title">加入会员</div>
-      <div className="buy-float-sub">解锁全部影视片段</div>
-      <div className="buy-float-badge">支持支付宝</div>
+      <div className="buy-float-title">点击这里</div>
+      <div className="buy-float-sub">解锁会员全部视频</div>
+      <div className="buy-float-badge">购买兑换码</div>
     </a>
   );
 }
